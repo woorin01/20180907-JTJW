07e3e001eeb13dbfd052ef8718b61e3a925b6520
JeongTaeLee <jeongtae8633@gmail.com>
2018-09-10 02:46:06 +0900

    2018_09_10

---
+++ 20180907-JTJW/Assets/Resource/Script/MapGenerated/MapGenerated.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerated : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform tile;
+    public int iMapSize;
+    
+    void Start () 
+    {
+        CreateMap();
+    }
+    void CreateMap()
+    {
+        string HolderName = "MapChild";
+
+        if ( transform.FindChild(HolderName) )
+            DestroyImmediate(transform.FindChild(HolderName).gameObject);
+
+        Transform MapHolder = new GameObject(HolderName).transform;
+        MapHolder.parent    = transform; 
+
+        Vector3 v3CreatePos = new Vector3(0f, 0f, 0f);
+        for (int size = 0; size < iMapSize; size++)
+        {
+            Transform createTile    = Instantiate(tile, transform) as Transform;
+            createTile.position     = v3CreatePos;
+            createTile.parent       = MapHolder;
+
+            v3CreatePos.z           += 4;
+        }
+    }
+}
