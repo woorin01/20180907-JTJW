de1f53c816ffa2ff88bf7778d24723d1d0d6fc8b
JeongTaeLee <jeongtae8633@gmail.com>
2018-09-25 01:12:12 +0900

    캐릭터 조작 인식 추가

--- 20180907-JTJW/Assets/Script/CharacterScript.cs
+++ 20180907-JTJW/Assets/Script/CharacterScript.cs
@@ -16,6 +16,9 @@ public class CharacterScript : MonoBehaviour {
     [Range(1, 20)]
     public float     fAniSpeedDivide;
 
+    private Vector2 v2StartTouchPos;
+    private bool    bTouchOn = false;
+
     // 컴포넌트 변수
     Animator PlayerAnimator;
 
@@ -30,15 +33,67 @@ public class CharacterScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        CharacterMove();
+        ForwardMovement();
+        UserOperation();
 	}
 
-    void CharacterMove()
+    // 사용자 함수 
+    void ForwardMovement()
     {
         transform.Translate(new Vector3(0, 0, fNowCharSpeed * Time.deltaTime));
     }
 
-    // 사용자 함수 
+    void UserOperation()
+    {
+        Vector2 v2NowTouchPos = new Vector2(0,0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            bTouchOn = true;
+            v2StartTouchPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if(bTouchOn)
+                v2NowTouchPos   = Input.mousePosition;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            bTouchOn        = false;
+            v2StartTouchPos = new Vector2(0, 0);
+        }
+
+        Vector2 v2TouchLength;
+        v2TouchLength.x = Mathf.Abs(v2NowTouchPos.x - v2StartTouchPos.x);
+        v2TouchLength.y = Mathf.Abs(v2NowTouchPos.y - v2StartTouchPos.y);
+
+        if(v2TouchLength.x > 10 || v2TouchLength.y > 10)
+        {
+            // 상하 조작
+            if(v2TouchLength.y > v2TouchLength.x)
+            {
+                // 점프인지
+                if(v2NowTouchPos.y > v2StartTouchPos.y)
+                    Debug.Log("J");
+                // 슬라이드인지                
+                if (v2NowTouchPos.y < v2StartTouchPos.y)
+                    Debug.Log("S");
+            }
+            // 좌우 조작
+            else if(v2TouchLength.y < v2TouchLength.x)
+            {
+                // 오른쪽인지
+                if(v2NowTouchPos.x > v2StartTouchPos.x)
+                    Debug.Log("R");
+                // 왼쪽인지
+                if (v2NowTouchPos.x < v2StartTouchPos.x)
+                    Debug.Log("L");
+            }
+
+            v2StartTouchPos = new Vector2(0,0);
+            bTouchOn = false;
+        }
+    }
 
 
     // 코루틴 함수
